690e1e6d2ac10381599fd83804a92dd062b8b37e
jmf42 <juanmanuelfontes@gmail.com>
2025-05-24 19:10:15 +0200

    Initial Commit

---
+++ WorthIt/WorthItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorthItApp.swift
+//  WorthIt
+//
+//  Created by Juan Manuel Fontes on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorthItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorthItTests/WorthItTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorthItTests.swift
+//  WorthItTests
+//
+//  Created by Juan Manuel Fontes on 24.05.2025.
+//
+
+import Testing
+@testable import WorthIt
+
+struct WorthItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
